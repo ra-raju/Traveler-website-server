b65403d1126fa9f3c22df12d2fec020ac8ccdf5a
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-10-29 08:32:06 +0600

    server setup success

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.DB_HOST || 8000;
+
+/* 
+traveler
+Y6Ogi1kIUXDTkFwi */
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wn1l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('i am working');
+});
+
+app.listen(port, () => {
+  console.log('server is running on port', port);
+});
